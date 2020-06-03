bf775be16313c9f87e73d3215c500e34445acfdd
Matheus Tenorio <matheus.santos153@senaisp.edu.br>
2020-06-03 09:14:12 -0300

    Adicionar arquivos de projeto.

---
+++ ConsumindoAPI_XF/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Xamarin.Forms;
+
+namespace ConsumindoAPI_XF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+        protected override void OnStart()
+        {
+        }
+        protected override void OnSleep()
+        {
+        }
+        protected override void OnResume()
+        {
+        }
+    }
+}
