123af90a9feefd5e9f66e54e4c4fed705468d848
Matheus Tenorio <matheus.santos153@senaisp.edu.br>
2020-06-03 09:54:45 -0300

    Adicionado o modelo Aluno;

---
+++ ConsumindoAPI_XF/Models/Aluno.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsumindoAPI_XF.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public Aluno(int Id, string Nome, string Sobrenome)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Sobrenome = Sobrenome;
+        }
+    }
+}
